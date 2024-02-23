878200fd1201d1b04eea7ff785a20b3bae193e64
okahiro0701 <okanaka19970701@gmail.com>
2024-02-23 15:31:38 +0900

    可読性の向上の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -19,18 +20,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     unless @item.user_id == current_user.id
       redirect_to action: :index
     end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to root_path
     else
@@ -42,5 +40,9 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:image, :item_name, :item_explanation, :price, :category_id, :item_condition_id, :shipping_fee_burden_id, :prefecture_id, :delivery_time_id).merge(user: current_user)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
