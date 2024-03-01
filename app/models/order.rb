acab6fe5a97a4d5c53a24c4a63b76279aa794049
okahiro0701 <okanaka19970701@gmail.com>
2024-03-02 06:45:16 +0900

    商品購入機能の実装

---
+++ app/models/order.rb
@@ -0,0 +1,5 @@
+class Order < ApplicationRecord
+  belongs_to :user
+  has_one :item
+  has_one :address
+end
---
+++ app/models/order_address.rb
@@ -0,0 +1,21 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :item_id, :user_id, :zip_code, :prefecture_id, :city_name, :block_number, :building_name, :phone_number, :price
+  attr_accessor :token
+
+  with_options presence: true do
+    validates :zip_code,  format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :phone_number, format: {with: /\A\d{11}\z/, message: "is invalid. disclude hyphen(-)"}
+    validates :city_name, :block_number
+    validates :token
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :prefecture_id, numericality: {message: "can't be blank"}
+  end
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(zip_code: zip_code, prefecture_id: prefecture_id, city_name: city_name, block_number: block_number, building_name: building_name, phone_number: phone_number, order_id: order.id)
+  end
+end
